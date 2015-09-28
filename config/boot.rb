c8dc2c1aa58f3ece32e60e5b442e9ddca8b967ce
Marko Anastasov <marko@renderedtext.com>
2015-09-28 12:51:56 +0200

    Use Postgres.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,24 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require "rubygems"
+require "yaml"
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
+
+require "bundler/setup" if File.exists?(ENV["BUNDLE_GEMFILE"])
+
+# Modify Rails 4 default IP binding, so that host machines can access the server
+require "rails/commands/server"
+
+module Rails
+
+  class Server
+
+    new_default_options = Module.new do
+
+      def default_options
+        super.merge(:Host => "0.0.0.0") if Rails.env == "development"
+      end
+    end
+
+    prepend new_default_options
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150312173037) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "confessions", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at", null: false
